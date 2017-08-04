1fc5112d1278335342188211a1aa9fff53518c25
Draven <jspankai@vip.qq.com>
2017-08-04 14:12:54 +0800

    format g.js

--- wechat/g.js
+++ wechat/g.js
@@ -2,57 +2,57 @@
 
 var sha1 = require('sha1')
 
-function Wechat(opts){
+function Wechat(opts) {
   var that = this
   this.appID = opts.appID
   this.appSecret = opts.appSecret
   this.getAccessToken = opts.getAccessToken
   this.saveAccessToken = opts.getAccessToken
 
- this.getAccessToken()
-.then(function(data){
- try{
-  data = JSON.parse(data)
-}catch(e){
- return that.updateAccessToken()
+  this.getAccessToken()
+    .then(function (data) {
+      try {
+        data = JSON.parse(data)
+      } catch (e) {
+        return that.updateAccessToken()
+      }
+
+      if (that.isValidAccessToken(data)) {
+        Promise.resolve(data)
+      } else {
+        return that.updateAccessToken()
+      }
+    })
+    .then(function (data) {
+      that.access_token = data.access_token
+      that.expires_in = data.expires_in
+
+      that.saveAccessToken(data)
+    })
 }
 
-if(that.isValidAccessToken(data)){
- Promise.resolve(data)
-}else{
- return that.updateAccessToken()
-}
-})
-.then(function(data){
- that.access_token = data.access_token
- that.expires_in = data.expires_in
+Wechat.prototype.isValidAccessToken = function (data) {
 
- that.saveAccessToken(data)
-})
-}
+  if (!data || !data.access_token || !data.expires_in) {
+    return false
+  }
 
-Wechat.prototype.isValidAccessToken = function(data){
-
-if(!data || !data.access_token || !data.expires_in){
- return false
+  var access_token = data.access_token
+  var expires_in = data.expires_in
+  var now = (new Date().getTime())
+  if (now < expires_in) {
+    return true
+  } else {
+    return false
+  }
 }
 
-var access_token = data.access_token
-var expires_in = data.expires_in
-var now = (new Date().getTime())
-if(now < expires_in){
- return true
-}else{
- return false
-}
-}
-
-Wechat.prototype.updateAccessToken = function(){
+Wechat.prototype.updateAccessToken = function () {
 
 }
 
-module.exports = function(opts) {
-  return function *(next){
+module.exports = function (opts) {
+  return function* (next) {
     console.log(this.query)
 
     var token = opts.token
@@ -60,13 +60,13 @@ module.exports = function(opts) {
     var nonce = this.query.nonce
     var timestamp = this.query.timestamp
     var echostr = this.query.echostr
-    var str = [token,timestamp,nonce].sort().join('')
+    var str = [token, timestamp, nonce].sort().join('')
     var sha = sha1(str)
 
-    if(sha === signature){
-        this.body = echostr + ''
-    }else{
-        this.body = 'wrong'
+    if (sha === signature) {
+      this.body = echostr + ''
+    } else {
+      this.body = 'wrong'
     }
   }
 }
