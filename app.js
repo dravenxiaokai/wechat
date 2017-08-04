168fbea4700450fc214620d4180444e497593668
draven <jspankai@vip.qq.com>
2017-08-04 14:04:52 +0800

    update g

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var Koa = require('koa')
-var sha1 = require('sha1')
+var wechat = require('./wechat/g');
 var config = {
     wechat: {
         appID: 'wx10634733a3237f78',
@@ -11,23 +11,7 @@ var config = {
 }
 var app = new Koa()
 
-app.use(function* (next) {
-    console.log(this.query)
-    var token = config.wechat.token
-    var signature = this.query.signature
-    var nonce = this.query.nonce
-    var timestamp = this.query.timestamp
-    var echostr = this.query.echostr
-
-    var str = [token,timestamp,nonce].sort().join('')
-    var sha = sha1(str)
-
-    if(sha === signature){
-        this.body = echostr + ''
-    }else{
-        this.body = 'wrong'
-    }
-})
+app.use(wechat(config.wechat))
 
 app.listen(3002, 'localhost', () => {
     console.log('listening:3002')
---
+++ wechat/g.js
@@ -0,0 +1,72 @@
+'use strict'
+
+var sha1 = require('sha1')
+
+function Wechat(opts){
+  var that = this
+  this.appID = opts.appID
+  this.appSecret = opts.appSecret
+  this.getAccessToken = opts.getAccessToken
+  this.saveAccessToken = opts.getAccessToken
+
+ this.getAccessToken()
+.then(function(data){
+ try{
+  data = JSON.parse(data)
+}catch(e){
+ return that.updateAccessToken()
+}
+
+if(that.isValidAccessToken(data)){
+ Promise.resolve(data)
+}else{
+ return that.updateAccessToken()
+}
+})
+.then(function(data){
+ that.access_token = data.access_token
+ that.expires_in = data.expires_in
+
+ that.saveAccessToken(data)
+})
+}
+
+Wechat.prototype.isValidAccessToken = function(data){
+
+if(!data || !data.access_token || !data.expires_in){
+ return false
+}
+
+var access_token = data.access_token
+var expires_in = data.expires_in
+var now = (new Date().getTime())
+if(now < expires_in){
+ return true
+}else{
+ return false
+}
+}
+
+Wechat.prototype.updateAccessToken = function(){
+
+}
+
+module.exports = function(opts) {
+  return function *(next){
+    console.log(this.query)
+
+    var token = opts.token
+    var signature = this.query.signature
+    var nonce = this.query.nonce
+    var timestamp = this.query.timestamp
+    var echostr = this.query.echostr
+    var str = [token,timestamp,nonce].sort().join('')
+    var sha = sha1(str)
+
+    if(sha === signature){
+        this.body = echostr + ''
+    }else{
+        this.body = 'wrong'
+    }
+  }
+}
